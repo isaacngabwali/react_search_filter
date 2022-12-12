353998885c8ee540da736b09805cd5dc9cebe1ec
isaacngabwali <31367648+isaacngabwali@users.noreply.github.com>
2022-12-12 11:37:23 +0300

    rsbase

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com'
+    baseURL: 'https://rsbase.fanitehub.com/api/v1/'
 })
 
 export const getPosts = async () => {
-    const response = await api.get('/posts')
+    const response = await api.get('/products')
     return response.data
 }
